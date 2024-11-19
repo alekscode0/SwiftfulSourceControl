730d8ad842c1390203c91cc7b31fd53c0ebd4ecf
Aleksandar <183597320+alekscode0@users.noreply.github.com>
2024-11-19 17:49:16 +0100

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Aleksandar on 19.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
