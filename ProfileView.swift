69868a63aa03d997d53b374ae66f3116a20d25f5
Aleksandar <183597320+alekscode0@users.noreply.github.com>
2024-12-01 15:51:20 +0100

    [Feature] Add Profile View

---
+++ ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Aleksandar on 1.12.24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Aleksandar")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
