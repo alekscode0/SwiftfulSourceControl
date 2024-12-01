14e9903b527590ef3a0348044ce92bdc944a02ae
Aleksandar <183597320+alekscode0@users.noreply.github.com>
2024-12-01 14:33:01 +0100

    [Patch] Fix incorrect text

--- HomeView.swift
+++ HomeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack {
-            Text("Screen")
+            Text("Screen1")
         }
         Button("Follow me", action: {
             
